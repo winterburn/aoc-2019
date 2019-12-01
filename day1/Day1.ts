89ef64aec8891847d28a971f764bb1b9ab02ed80
Olli Helminen <o.helminen@gmail.com>
2019-12-01 15:06:15 +0200

    Day one second part

--- day1/Day1.ts
+++ day1/Day1.ts
@@ -1,12 +1,20 @@
 import * as fs from 'fs';
 import * as rd from 'readline';
-var reader = rd.createInterface(fs.createReadStream('input_day1.txt'));
+var reader = rd.createInterface(fs.createReadStream('day1/input_day1.txt'));
 var data: Array<string> = [];
-var fuel_required = 0;
+var fuel_required: number = 0;
 reader.on('line', (mass:string) => {
-    fuel_required += Math.floor(Number(mass) / 3) - 2;
+    fuel_required = fuel_required + calculate_fuel(Number(mass));
     console.log(fuel_required);
 });
 reader.on('close', () => {
     console.log(`final fuel required: ${fuel_required}`)
 })
+
+var calculate_fuel = (mass: number): number => {
+    let fuel = Math.floor(mass / 3) - 2;
+    if (fuel > 0) {
+        return fuel + calculate_fuel(fuel);
+    }
+    return 0;
+};
--- out/Day1.js
+++ out/Day1.js
@@ -2,13 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const rd = require("readline");
-var reader = rd.createInterface(fs.createReadStream('input_day1.txt'));
+var reader = rd.createInterface(fs.createReadStream('day1/input_day1.txt'));
 var data = [];
 var fuel_required = 0;
 reader.on('line', (mass) => {
-    fuel_required += Math.floor(Number(mass) / 3) - 2;
+    fuel_required = fuel_required + calculate_fuel(Number(mass));
     console.log(fuel_required);
 });
 reader.on('close', () => {
     console.log(`final fuel required: ${fuel_required}`);
 });
+var calculate_fuel = (mass) => {
+    let fuel = Math.floor(mass / 3) - 2;
+    if (fuel > 0) {
+        return fuel + calculate_fuel(fuel);
+    }
+    return 0;
+};
